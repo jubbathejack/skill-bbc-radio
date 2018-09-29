0cf919355bf52d3166a633e55dfaef8bc6bc6c51
Jack Parker <jack@jubba.org>
2018-09-29 21:15:12 +0100

    Will this work?

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
-# skill-bbc-radio
+# mycroft-skill-bbc-news
 #
-# A Mycroft skill to play BBC radio stations. Currently only configured
-# for Radio 1 whilst I figure out how to select multiple from array of
-# some sort.
+# A Mycroft skill to play the latest bbc news
 #
 # Based on the mycroft-skill-cbc-news skill by chrison999
 #
@@ -12,8 +10,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this skill.  If not, see <http://www.gnu.org/licenses/>.
 
+
+import feedparser
 import time
 from os.path import dirname
+import re
 
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
@@ -25,33 +26,34 @@ __author__ = 'jubbathejack'
 LOGGER = getLogger(__name__)
 
 
-class BBCRadioSkill(MycroftSkill):
+class BBCNewsSkill(MycroftSkill):
     def __init__(self):
-        super(BBCRadioSkill, self).__init__(name="BBCRadioSkill")
+        super(BBCNewsSkill, self).__init__(name="BBCNewsSkill")
         self.process = None
 
     def initialize(self):
-        intent = IntentBuilder("BBCRadioIntent").require(
-            "BBCRadioKeyword").build()
+        intent = IntentBuilder("BBCNewsIntent").require(
+            "BBCNewsKeyword").build()
         self.register_intent(intent, self.handle_intent)
 
     def handle_intent(self, message):
         try:
-            rstation = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p"
-            self.speak_dialog('start.radio')
+
+            data = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p"
+            self.speak_dialog('bbc.news')
             time.sleep(5)
 
-            self.process = play_mp3(rstation)
+            self.process = play_mp3(data)
 
         except Exception as e:
             LOGGER.error("Error: {0}".format(e))
 
     def stop(self):
         if self.process and self.process.poll() is None:
-            self.speak_dialog('stop.radio')
+            self.speak_dialog('bbc.news.stop')
             self.process.terminate()
             self.process.wait()
 
 
 def create_skill():
-    return BBCRadioSkill()
+    return BBCNewsSkill()
